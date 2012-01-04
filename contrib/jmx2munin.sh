b300a1e570a503f468deb50b609b98579172d526
Markus Alexander Kuppe <t-marku@SVC-LOANER-NO2.(none)>
2012-01-04 11:30:45 -0800

    Support for tlc2.tool:type=ModelChecker

--- contrib/jmx2munin.sh
+++ contrib/jmx2munin.sh
@@ -4,13 +4,16 @@
 #%# family=auto
 #%# capabilities=autoconf
 #
-# [cassandra_nodes_in_cluster]
-# env.url service:jmx:rmi:///jndi/rmi://127.0.0.1:5400/jmxrmi
-# env.query org.apache.cassandra.*:*
-# env.config cassandra/nodes_in_cluster
-# sets the 'config' and 'query' and 'url' variables for this script
 
-query='tlc2.*:*'
+set -x
+
+# cut off the part after _ in symlink name
+scriptname=${0##*/}
+
+jmxfuncUpper=${scriptname##*_}
+jmxfunc=${jmxfuncUpper,,}
+
+query='tlc2.*:type='$jmxfuncUpper
 config='tlc2/tlc'
 
 if [ -z "$MUNIN_LIBDIR" ]; then
@@ -40,7 +43,58 @@ JMX2MUNIN_DIR="/etc/munin/plugin-conf.d"
 CONFIG="$JMX2MUNIN_DIR/$config"
 
 if [ "$1" = "config" ]; then
-    cat "$CONFIG"
+
+
+case "$jmxfunc" in
+    *modelchecker*)
+cat <<__EOF
+graph_title $jmxfuncUpper
+graph_scale yes
+graph_category tlc
+graph_vlabel statesGenerated
+graph_vlabel statesGeneratedPerMinute
+graph_vlabel distinctStatesGenerated
+graph_vlabel distinctStatesGeneratedPerMinute
+graph_vlabel stateQueueSize
+graph_vlabel progressLevel
+__EOF
+echo tlc2_tool_"$jmxfunc"_distinctstatesgenerated.label distinctStatesGenerated
+echo tlc2_tool_"$jmxfunc"_progress.label progressLevel
+echo tlc2_tool_"$jmxfunc"_distinctstatesgeneratedperminute.label distinctStatesGeneratedPerMinute
+echo tlc2_tool_"$jmxfunc"_statequeuesize.label stateQueueSize
+echo tlc2_tool_"$jmxfunc"_statesgenerated.label statesGenerated
+echo tlc2_tool_"$jmxfunc"_statesgeneratedperminute.label statesGeneratedPerMinute
+    ;;
+
+    *diskfpset*)
+cat <<__EOF
+graph_title $jmxfuncUpper
+graph_scale yes
+graph_category tlc
+graph_vlabel tblCnt
+graph_vlabel fileCnt
+graph_vlabel indexCnt
+graph_vlabel diskLookupCnt
+graph_vlabel memHitCnt
+graph_vlabel diskHitCnt
+graph_vlabel diskWriteCnt
+graph_vlabel diskSeekCnt
+graph_vlabel growDiskMark
+graph_vlabel checkpointMark
+__EOF
+echo tlc2_tool_fp_"$jmxfunc"_filecnt.label fileCnt
+echo tlc2_tool_fp_"$jmxfunc"_indexcnt.label indexCnt
+echo tlc2_tool_fp_"$jmxfunc"_disklookupcnt.label diskLookupCnt
+echo tlc2_tool_fp_"$jmxfunc"_memhitcnt.label memHitCnt
+echo tlc2_tool_fp_"$jmxfunc"_diskhitcnt.label diskHitCnt
+echo tlc2_tool_fp_"$jmxfunc"_diskwritecnt.label diskWriteCnt
+echo tlc2_tool_fp_"$jmxfunc"_diskseekcnt.label diskSeekCnt
+echo tlc2_tool_fp_"$jmxfunc"_tblcnt.label tblCnt
+echo tlc2_tool_fp_"$jmxfunc"_growdiskmark.label growDiskMark
+echo tlc2_tool_fp_"$jmxfunc"_checkpointmark.label checkpointMark
+    ;;
+esac
+    #cat "$CONFIG"
     exit 0
 fi
 
