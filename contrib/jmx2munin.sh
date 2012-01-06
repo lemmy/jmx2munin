855a052494d11d9ecb1445aa5ddff4649c3c4c48
Markus Alexander Kuppe <bugs.eclipse.org@lemmster.de>
2012-01-06 11:23:44 -0800

    Add support for one graph per value

--- contrib/jmx2munin.sh
+++ contrib/jmx2munin.sh
@@ -9,11 +9,24 @@
 
 # cut off the part after _ in symlink name
 scriptname=${0##*/}
+jmxfunc=${scriptname##*_}
 
-jmxfuncUpper=${scriptname##*_}
-jmxfunc=${jmxfuncUpper,,}
+# string manipulation (remove from front and back)
+package_with_class=${jmxfunc%%::*}
+package=${package_with_class%:*}
+package_with_underscore=${package//:/_}
+package_with_dots=${package//:/.}
 
-query='tlc2.*:type='$jmxfuncUpper
+# split of class name
+class=${package_with_class##*:}
+class_lower=${class,,}
+
+# split of attribute part
+attribute=${jmxfunc##*::}
+
+# create query string
+query=$package_with_dots':type='$class
+
 config='tlc2/tlc'
 
 if [ -z "$MUNIN_LIBDIR" ]; then
@@ -24,103 +37,50 @@ if [ -f "$MUNIN_LIBDIR/plugins/plugin.sh" ]; then
     . $MUNIN_LIBDIR/plugins/plugin.sh
 fi
 
+# we support munin autoconf
 if [ "$1" = "autoconf" ]; then
     echo yes
     exit 0
 fi
 
+# this is very common so make it a default
 if [ -z "$url" ]; then
-  # this is very common so make it a default
   url="service:jmx:rmi:///jndi/rmi://127.0.0.1:5400/jmxrmi"
 fi
 
-if [ -z "$config" -o -z "$query" -o -z "$url" ]; then
-  echo "Configuration needs attributes config, query and optinally url"
+if [ -z "$query" -o -z "$url" ]; then
+  echo "Configuration needs attributes query and optinally url"
   exit 1
 fi
 
 JMX2MUNIN_DIR="/etc/munin/plugin-conf.d"
-CONFIG="$JMX2MUNIN_DIR/$config"
 
+# if config as parameter request, munin wants to know how to layout this graph
 if [ "$1" = "config" ]; then
-
-
-case "$jmxfunc" in
-    *modelchecker*)
-cat <<__EOF
-graph_title $jmxfuncUpper
-graph_scale yes
-graph_category tlc
-graph_vlabel statesGenerated
-graph_vlabel statesGeneratedPerMinute
-graph_vlabel distinctStatesGenerated
-graph_vlabel distinctStatesGeneratedPerMinute
-graph_vlabel stateQueueSize
-graph_vlabel progressLevel
-__EOF
-echo tlc2_tool_"$jmxfunc"_distinctstatesgenerated.label distinctStatesGenerated
-echo tlc2_tool_"$jmxfunc"_progress.label progressLevel
-echo tlc2_tool_"$jmxfunc"_distinctstatesgeneratedperminute.label distinctStatesGeneratedPerMinute
-echo tlc2_tool_"$jmxfunc"_statequeuesize.label stateQueueSize
-echo tlc2_tool_"$jmxfunc"_statesgenerated.label statesGenerated
-echo tlc2_tool_"$jmxfunc"_statesgeneratedperminute.label statesGeneratedPerMinute
-    ;;
-
-    *diskfpset*)
-cat <<__EOF
-graph_title $jmxfuncUpper
-graph_scale yes
-graph_category tlc
-graph_vlabel tblCnt
-graph_vlabel fileCnt
-graph_vlabel indexCnt
-graph_vlabel diskLookupCnt
-graph_vlabel memHitCnt
-graph_vlabel diskHitCnt
-graph_vlabel diskWriteCnt
-graph_vlabel diskSeekCnt
-graph_vlabel growDiskMark
-graph_vlabel checkpointMark
-__EOF
-echo tlc2_tool_fp_"$jmxfunc"_filecnt.label fileCnt
-echo tlc2_tool_fp_"$jmxfunc"_indexcnt.label indexCnt
-echo tlc2_tool_fp_"$jmxfunc"_disklookupcnt.label diskLookupCnt
-echo tlc2_tool_fp_"$jmxfunc"_memhitcnt.label memHitCnt
-echo tlc2_tool_fp_"$jmxfunc"_diskhitcnt.label diskHitCnt
-echo tlc2_tool_fp_"$jmxfunc"_diskwritecnt.label diskWriteCnt
-echo tlc2_tool_fp_"$jmxfunc"_diskseekcnt.label diskSeekCnt
-echo tlc2_tool_fp_"$jmxfunc"_tblcnt.label tblCnt
-echo tlc2_tool_fp_"$jmxfunc"_growdiskmark.label growDiskMark
-echo tlc2_tool_fp_"$jmxfunc"_checkpointmark.label checkpointMark
-    ;;
-esac
-    #cat "$CONFIG"
+                         echo graph_title $class $attribute
+    echo graph_scale yes
+    echo graph_category tlc
+    echo graph_vlabel $attribute
+    echo $package_with_underscore'_'$class_lower'_'$attribute.label $attribute
     exit 0
 fi
 
 JAR="/usr/share/munin/jmx2munin.jar"
-CACHED="/tmp/jmx2munin"$jmxfuncUpper
+CACHED="/tmp/jmx2munin_"$package_with_dots'.'$class'#'$attribute
 
+# refresh cached file if necessary
 if test ! -f $CACHED || test `find "$CACHED" -mmin +2`; then
 
     java -jar "$JAR" \
       -url "$url" \
       -query "$query" \
-      $ATTRIBUTES \
       > $CACHED
 
     echo "cached.value `date +%s`" >> $CACHED
 fi
 
-ATTRIBUTES=`awk '/\.label/ { gsub(/\.label/,""); print $1 }' $CONFIG`
-
-if [ -z "$ATTRIBUTES" ]; then
-  echo "Could not find any *.label lines in $CONFIG"
-  exit 1
-fi
-
-for ATTRIBUTE in $ATTRIBUTES; do
-  grep $ATTRIBUTE $CACHED
-done
+# read fresh or cached value from cache file
+ATTRIBUTE=$package_with_underscore'_'$class_lower'_'$attribute.value
+grep $ATTRIBUTE $CACHED
 
 exit 0
